f2f9f588f8128b1f5674a3c923adf3865e753307
GANG_FANG <gfang1212@gmail.com>
2018-10-17 09:06:35 +0800

    Set up ImageGalleryCollectionViewController

---
+++ ImageGallery/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by GANG_FANG on 2018/10/16.
+//  Copyright © 2018 gfang. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ ImageGallery/ImageGalleryCollectionViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ImageGalleryCollectionViewController.swift
+//  ImageGallery
+//
+//  Created by GANG_FANG on 2018/10/16.
+//  Copyright © 2018 gfang. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCollectionViewController: UICollectionViewController {
+
+    let images = [UIImage(named: "model3"), UIImage(named: "model3")]
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath)
+    
+        if let imageCell = cell as? ImageCollectionViewCell, images[indexPath.item] != nil {
+            imageCell.imageView.image = images[indexPath.item]
+        }
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
