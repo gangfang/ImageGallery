5f7c2fe893a0b084779d17c86fcf7749d16fba0a
GANG_FANG <gfang1212@gmail.com>
2018-10-18 14:39:26 +0800

    Implement dragging and dropping for ImageGalleryCollectionViewController

--- ImageGallery/ImageGalleryCollectionViewController.swift
+++ ImageGallery/ImageGalleryCollectionViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-class ImageGalleryCollectionViewController: UICollectionViewController {
+class ImageGalleryCollectionViewController: UICollectionViewController, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
 
-    let images = [UIImage(named: "model3"), UIImage(named: "model3")]
+    var images = [UIImage(named: "model3"), UIImage(named: "elon")]
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.dragDelegate = self
+        collectionView?.dropDelegate = self
     }
-    */
-
+    
+    
+    
     // MARK: UICollectionViewDataSource
-
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath)
     
@@ -39,6 +38,86 @@ class ImageGalleryCollectionViewController: UICollectionViewController {
         return cell
     }
 
+    
+    
+    // MARK: UICollectionViewDragDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+    }
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
+        if let draggedImage = (collectionView?.cellForItem(at: indexPath) as? ImageCollectionViewCell)?.imageView.image {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: draggedImage))
+            dragItem.localObject = draggedImage
+            return [dragItem]
+        } else {
+            return []
+        }
+    }
+    
+    
+    // MARK: UICollectionViewDropDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        if isDragFromWithin(of: session) {
+            return session.canLoadObjects(ofClass: UIImage.self)
+        } else {
+            return session.canLoadObjects(ofClass: UIImage.self) && session.canLoadObjects(ofClass: URL.self)
+        }
+    }
+    // go back and check .move and .copy after implementing the move drop
+    func collectionView(_ collectionView: UICollectionView,
+                        dropSessionDidUpdate session: UIDropSession,
+                        withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        return UICollectionViewDropProposal(operation: isDragFromWithin(of: session) ? .move: .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    private func isDragFromWithin(of session: UIDropSession) -> Bool {
+        return (session.localDragSession?.localContext as? UICollectionView) == collectionView
+    }
+    
+    // to prevent app from crashing of 'attempt to begin reordering on collection view while reordering is already in progress'
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidEnd session: UIDropSession) {
+        collectionView.endInteractiveMovement()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+        for item in coordinator.items {
+            if let sourceIndexPath = item.sourceIndexPath {     // try using func isDragFromWithin
+                if let draggedImage = item.dragItem.localObject as? UIImage {
+                    collectionView.performBatchUpdates({
+                        images.remove(at: sourceIndexPath.item)     // to update the model
+                        images.insert(draggedImage, at: destinationIndexPath.item)
+                        collectionView.deleteItems(at: [sourceIndexPath])   // to update the view
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    })
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath) // for animation
+                }
+            } else {
+                let placeHolderContext = coordinator.drop(item.dragItem,
+                                                          to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "dropPlaceholderCell"))
+                item.dragItem.itemProvider.loadObject(ofClass: UIImage.self) { (provider, error) in
+                    DispatchQueue.main.async {
+                        if let image = provider as? UIImage {
+                            placeHolderContext.commitInsertion(dataSourceUpdates: { (insertionIndexPath) in
+                                self.images.insert(image, at: insertionIndexPath.item)
+                            })
+                        } else {
+                            placeHolderContext.deletePlaceholder()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
     // MARK: UICollectionViewDelegate
 
     /*
@@ -70,4 +149,15 @@ class ImageGalleryCollectionViewController: UICollectionViewController {
     }
     */
 
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
